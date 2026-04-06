e102ab8e5c32c30b72eedd5e5aff73701b605bc4
Yash Nagpal <soiidux040@gmail.com>
2026-04-06 22:34:22 +0530

    Added Logout button on Navbar and fixed useEffect dependencies in home.tsx

--- app/components/Navbar.tsx
+++ app/components/Navbar.tsx
@@ -1,14 +1,21 @@
+import React from "react";
 import { Link } from "react-router";
-
+import { usePuterStore } from "~/lib/puter";
 export default function Navbar() {
+  const { auth } = usePuterStore();
   return (
     <nav className="navbar">
       <Link to="/">
         <p className="text-2xl font-bold text-gradient">ResuAI</p>
       </Link>
-      <Link to="/upload" className="primary-button w-fit">
-        Upload Resume
-      </Link>
+      <div className="flex flex-row gap-2">
+        <Link to="/upload" className="primary-button w-fit">
+          Upload Resume
+        </Link>
+        <button className="primary-button w-fit" onClick={async (e) => { e.preventDefault(); await auth.signOut(); }}>
+          Logout
+        </button>
+      </div>
     </nav>
   );
 }
